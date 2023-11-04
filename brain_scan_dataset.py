e6b444c7428dd692b9ad91ab8ae96178856074e2
Nathan <nathansidi@icloud.com>
2023-11-04 12:58:21 +0000

    markdown 3 in 8 try 1

---
+++ brain_scan_dataset.py
@@ -0,0 +1,89 @@
+# brain_scan_dataset.py
+
+import os
+import json
+import skimage.io
+import numpy as np
+from mrcnn import utils
+
+class BrainScanDataset(utils.Dataset):
+
+    def load_brain_scan(self, dataset_dir, subset):
+        """Load a subset of the FarmCow dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("tumor", 1, "tumor")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val", 'test']
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        annotations = json.load(open(os.path.join(DATASET_DIR, subset, 'annotations_'+subset+'.json')))
+        annotations = list(annotations.values())  # don't need the dict keys
+
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+
+        # Add images
+        for a in annotations:
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. These are stores in the
+            # shape_attributes (see json format above)
+            # The if condition is needed to support VIA versions 1.x and 2.x.
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']]
+
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            image_path = os.path.join(dataset_dir, a['filename'])
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+
+            self.add_image(
+                "tumor",
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, 
+                height=height,
+                polygons=polygons
+            )
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a farm_cow dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "tumor":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            mask[rr, cc, i] = 1
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "tumor":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
