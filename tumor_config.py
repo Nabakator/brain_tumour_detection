d73bf52fa651ef171e731f8c3c321f6c9ef60086
Nabakator <nathansidi@icloud.com>
2023-11-04 22:30:54 +0000

    ok

--- tumor_config.py
+++ tumor_config.py
@@ -1,8 +1,8 @@
 from mrcnn.config import Config
 
 class TumorConfig(Config):
-    """Configuration for training on the brain tumor dataset.
-    """
+    """Configuration for training on the brain tumor dataset."""
+    
     # Give the configuration a recognizable name
     NAME = 'tumor_detector'
     GPU_COUNT = 1
@@ -11,3 +11,6 @@ class TumorConfig(Config):
     DETECTION_MIN_CONFIDENCE = 0.85    
     STEPS_PER_EPOCH = 100
     LEARNING_RATE = 0.001
+
+    # Add the missing attribute
+    PRE_NMS_LIMIT = 600
